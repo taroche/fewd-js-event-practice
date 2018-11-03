614e3b534c7089c7b4094db477c41cfc108acbbf
Tristan LaRoche <laroche11235@gmail.com>
2018-11-03 13:10:51 -0400

    add click to second button to change text

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,10 @@
       buttons[0].addEventListener('click', function () {
         buttons[0].classList.toggle('button-danger');
       })
+      buttons[1].addEventListener('click', changeText);
+
+      function changeText() {
+        buttons[1].innerText = 'you click me';
+      }
 
 })();
