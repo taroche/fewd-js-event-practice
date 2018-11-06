6d987734fa152e67362121341c0e682fc0964485
Tristan LaRoche <laroche11235@gmail.com>
2018-11-06 18:25:18 -0500

    cleaned up code

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,18 @@
 (function () {
-  var row = document.querySelector('main > div');
+
   var buttons = document.querySelectorAll('button');
 
-  row.addEventListener('click', handleEvent);
+  document.documentElement.addEventListener('click', handleEvent);
 
   function handleEvent (event) {
     if (event.target === buttons[0]){
-      event.target.classList.toggle('button-danger');
+      buttons[0].classList.toggle('button-danger');
     }
     if (event.target === buttons[1]){
-      event.target.innerText = 'you clicked me'
+      buttons[1].innerText = 'you clicked me';
     }
     if (event.target === buttons[2]){
-      buttons[1].innerText = 'button two'
+      buttons[1].innerText = 'button two';
     }
   }
 
